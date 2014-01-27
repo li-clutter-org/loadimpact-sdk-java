c0bc1ae5446a5a5fd8a15a9ed850bce7f78da2fc
Robin Gustafsson <robin@loadimpact.com>
2014-01-27 12:29:52 +0100

    Fix missing versions in pom file and fix dateutils test cases breaking on non-CEST based systems.

--- src/test/java/com/loadimpact/util/DateUtilsTest.java
+++ src/test/java/com/loadimpact/util/DateUtilsTest.java
@@ -20,25 +20,28 @@ public class DateUtilsTest {
     @Test
     public void testToDateFromIso8601() {
         String input = "2013-09-09T02:34:51+00:00";
-        GregorianCalendar expected = new GregorianCalendar(2013, 9 - 1, 9, 2 + 2, 34, 51);
+        GregorianCalendar expected = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        expected.set(2013, 9 - 1, 9, 2, 34, 51);
         assertThat(clearMilliSecs(DateUtils.toDateFromIso8601(input)), is(clearMilliSecs(expected.getTime())));
     }
 
     @Test
     public void testToIso8601() throws Exception {
-        GregorianCalendar date = new GregorianCalendar(2013, 9 - 1, 9, 2, 34, 51);
+        GregorianCalendar date = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
+        date.set(2013, 9 - 1, 9, 2, 34, 51);
         date.setTimeZone(TimeZone.getTimeZone("CET"));
         assertThat(DateUtils.toIso8601(date.getTime()), is("2013-09-09T02:34:51+02:00"));
     }
 
     @Test
     public void testToDateFromTimestamp() {
-        GregorianCalendar expected = new GregorianCalendar(2013, 8, 10, 17, 38, 36);
+        GregorianCalendar expected = new GregorianCalendar(TimeZone.getTimeZone("CET"));
+        expected.set(2013, 9 - 1, 10, 17, 38, 36);
         assertThat(clearMilliSecs(DateUtils.toDateFromTimestamp(1378827516043595L)), is(clearMilliSecs(expected.getTime())));
     }
 
     private Date clearMilliSecs(Date date) {
-        GregorianCalendar c = new GregorianCalendar();
+        GregorianCalendar c = new GregorianCalendar(TimeZone.getTimeZone("UTC"));
         c.setTime(date);
         c.clear(Calendar.MILLISECOND);
         return c.getTime();
