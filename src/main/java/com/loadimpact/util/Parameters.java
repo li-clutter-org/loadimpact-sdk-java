49af80b768c5453266414108b0d30a0fc01b8cef
ribomation <jens.riboe@ribomation.com>
2015-06-26 15:16:41 +0200

    Fixed missing default param

--- src/main/java/com/loadimpact/util/Parameters.java
+++ src/main/java/com/loadimpact/util/Parameters.java
@@ -30,9 +30,10 @@ public class Parameters {
     public int size() {
         return parameters.size();
     }
-    
+
     /**
      * Adds a parameter
+     *
      * @param key   its key
      * @param value its value
      */
@@ -42,6 +43,7 @@ public class Parameters {
 
     /**
      * Returns all keys.
+     *
      * @return set of keys
      */
     public Set<String> keys() {
@@ -50,7 +52,8 @@ public class Parameters {
 
     /**
      * Returns all keys matching the given pattern.
-     * @param pattern   key pattern
+     *
+     * @param pattern key pattern
      * @return set of keys
      */
     public Set<String> keys(String pattern) {
@@ -63,7 +66,8 @@ public class Parameters {
 
     /**
      * Returns true if key is a member.
-     * @param key   key to check
+     *
+     * @param key key to check
      * @return true if member
      */
     public boolean has(String key) {
@@ -72,44 +76,53 @@ public class Parameters {
 
     /**
      * Returns the value associated with the key, or the given default value.
-     * @param key   key to find
+     *
+     * @param key          key to find
      * @param defaultValue if not found
      * @return value
      */
     public String get(String key, String defaultValue) {
-        return parameters.get(key);
+        String value = parameters.get(key);
+        return StringUtils.isBlank(value) ? defaultValue : value;
     }
 
     public int get(String key, int defaultValue) {
         try {
             return Integer.parseInt(get(key, NULL_String));
-        } catch (Exception e) { return defaultValue; }
+        } catch (Exception e) {
+            return defaultValue;
+        }
     }
 
     public boolean get(String key, boolean defaultValue) {
         try {
             return Boolean.parseBoolean(get(key, NULL_String));
-        } catch (Exception e) { return defaultValue; }
+        } catch (Exception e) {
+            return defaultValue;
+        }
     }
 
     public float get(String key, float defaultValue) {
         try {
             return Float.parseFloat(get(key, NULL_String));
-        } catch (Exception e) { return defaultValue; }
+        } catch (Exception e) {
+            return defaultValue;
+        }
     }
 
     public StandardMetricResult.Metrics get(String key, StandardMetricResult.Metrics defaultValue) {
         try {
             return StandardMetricResult.Metrics.valueOf(get(key, defaultValue.name()).toUpperCase());
-        } catch (Exception e) { 
+        } catch (Exception e) {
             e.printStackTrace();
-            return defaultValue; }
+            return defaultValue;
+        }
     }
 
     public DelayUnit get(String key, DelayUnit defaultValue) {
         return DelayUnit.valueOf(get(key, defaultValue.name()));
     }
-    
+
     public Operator get(String key, Operator defaultValue) {
         return Operator.valueOf(get(key, defaultValue.name()));
     }
